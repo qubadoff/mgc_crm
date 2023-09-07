63c9aa84ba1c3f417ee6a4cc806ee22ccc945cba
qubadoff <siyaset007@gmail.com>
2023-09-07 14:01:41 +0400

    Filter make

--- app/Filament/Resources/TimesheetResource.php
+++ app/Filament/Resources/TimesheetResource.php
@@ -83,7 +83,7 @@ class TimesheetResource extends Resource
                     Tables\Columns\TextColumn::make('working_hours')->searchable()->sortable(),
                     Tables\Columns\TextColumn::make('working_day')->searchable()->sortable()->date('d/m/Y'),
                     Tables\Columns\TextColumn::make('work_desc')->searchable()->sortable(),
-                    Tables\Columns\TextColumn::make('created_at')->searchable()->sortable()->date(),
+                    Tables\Columns\TextColumn::make('created_at')->searchable()->sortable()->date('d/m/Y'),
                 ])
                 ->filters([
                     Filter::make('working_day')
